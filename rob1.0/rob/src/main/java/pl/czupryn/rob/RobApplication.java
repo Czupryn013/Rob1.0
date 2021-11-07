300ab5accb1655c6a61926f64332ba928cef9585
Czupryn013 <93874434+Czupryn013@users.noreply.github.com>
2021-11-07 16:42:34 +0100

    Add files via upload

---
+++ rob1.0/rob/src/main/java/pl/czupryn/rob/RobApplication.java
@@ -0,0 +1,13 @@
+package pl.czupryn.rob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobApplication.class, args);
+	}
+
+}
